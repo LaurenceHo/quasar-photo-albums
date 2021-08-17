edd4563c49db12e5220882f261029dc3feeff5ee
Laurence Ho <bluegray.he@aol.com>
2021-08-18 09:54:37 +1200

    Add album list layout

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,44 +1,69 @@
 <template>
-  <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-6">
+  <div v-if="albumType === 'square'" class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-6">
     <q-img
       :ratio="1"
       :src="`${thumbnail[0].url}?tr=w-250,h-250`"
       class="rounded-borders album-thumbnail"
       @click="goToAlbum"
     >
-      <div class="absolute-bottom text-center">{{ albumName }}</div>
+      <div class="absolute-bottom text-center">{{ albumItem.albumName }}</div>
     </q-img>
   </div>
+  <template v-else>
+    <q-item clickable v-ripple @click="goToAlbum">
+      <q-item-section avatar>
+        <q-avatar rounded size="72px">
+          <q-img class="rounded-borders" :ratio="1" :src="`${thumbnail[0].url}?tr=w-72,h-72`" />
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label class="text-weight-medium">{{ albumItem.albumName }}</q-item-label>
+        <q-item-label v-if="albumItem.desc" caption>
+          {{ albumItem.desc }}
+        </q-item-label>
+        <div class="flex">
+          <q-chip v-for="tag in albumItem.tags" :key="tag" color="secondary" dense square>{{ tag }}</q-chip>
+        </div>
+      </q-item-section>
+    </q-item>
+  </template>
 </template>
 
 <script lang="ts">
 import { getPhotoObjectFromS3 } from 'components/helper';
 import { Photo } from 'components/models';
-import { defineComponent, onMounted, ref, toRefs } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'Album',
 
   props: {
-    albumName: {
+    albumType: {
       type: String,
       required: true,
+    },
+    albumItem: {
+      type: Object,
+      required: true,
+      default: () => ({ albumName: '', desc: '', tags: [] }),
     },
   },
 
   setup(props) {
-    const { albumName } = toRefs(props);
     const router = useRouter();
     const thumbnail = ref([] as Photo[]);
     thumbnail.value.push({ url: '' });
 
     // Get first photo of album as album cover
-    onMounted(() => getPhotoObjectFromS3(albumName.value, 1, (photos: Photo[]) => (thumbnail.value = photos)));
+    onMounted(() =>
+      getPhotoObjectFromS3(props.albumItem.albumName, 1, (photos: Photo[]) => (thumbnail.value = photos))
+    );
 
     return {
       thumbnail,
-      goToAlbum: () => router.push(`/album/${albumName.value}`),
+      goToAlbum: () => router.push(`/album/${props.albumItem.albumName}`),
     };
   },
 });
--- src/pages/AlbumList.vue
+++ src/pages/AlbumList.vue
@@ -1,12 +1,30 @@
 <template>
   <q-page class="q-pt-md">
     <div class="q-pb-md row justify-between items-center">
-      <q-input v-model="searchKey" dense outlined>
-        <template v-slot:append>
-          <q-icon name="mdi-magnify" />
-        </template>
-      </q-input>
-      <div class="row items-center">
+      <div class="col-12 col-xl-3 col-lg-4 col-md-4 flex">
+        <q-btn-group outline class="q-mb-sm">
+          <q-btn
+            :outline="albumListType === 'square'"
+            color="primary"
+            dense
+            icon="mdi-format-list-bulleted-square"
+            @click="setAlbumListType('list')"
+          />
+          <q-btn
+            :outline="albumListType === 'list'"
+            color="primary"
+            dense
+            icon="mdi-view-grid"
+            @click="setAlbumListType('square')"
+          />
+        </q-btn-group>
+        <q-input v-model="searchKey" class="full-width" dense outlined>
+          <template v-slot:append>
+            <q-icon name="mdi-magnify" />
+          </template>
+        </q-input>
+      </div>
+      <div class="col-12 col-xl-4 col-lg-5 col-md-5 flex justify-end items-center q-pt-md">
         <q-pagination
           v-model="pageNumber"
           :max="totalPages"
@@ -20,15 +38,22 @@
         ({{ totalItems }} albums)
       </div>
     </div>
-    <div class="q-col-gutter-md row items-start">
-      <Album v-for="album in chunkAlbumList" :key="album.albumName" :album-name="album.albumName" />
+    <div v-if="albumListType === 'square'" class="q-col-gutter-md row">
+      <Album v-for="album in chunkAlbumList" :key="album.albumName" :albumItem="album" :albumType="albumListType" />
+    </div>
+    <div v-else class="justify-center row">
+      <div class="col-12 col-xl-6 col-lg-8 col-md-8 column">
+        <q-list bordered separator class="rounded-borders">
+          <Album v-for="album in chunkAlbumList" :key="album.albumName" :albumItem="album" :albumType="albumListType" />
+        </q-list>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import Album from 'components/Album.vue';
-import { Album as AlbumName } from 'components/models';
+import { Album as AlbumItem } from 'components/models';
 import isEmpty from 'lodash/isEmpty';
 import { useStore } from 'src/store';
 import { computed, defineComponent, ref, watch } from 'vue';
@@ -45,9 +70,10 @@ export default defineComponent({
     const itemsPerPage = ref(20);
     const pageNumber = ref(1);
     const searchKey = ref('');
+    const albumListType = ref('list');
 
     const totalItems = ref(store.state.allAlbumList.length);
-    const chunkAlbumList = ref(store.getters.chunkAlbumList(0, itemsPerPage.value) as AlbumName[]);
+    const chunkAlbumList = ref(store.getters.chunkAlbumList(0, itemsPerPage.value) as AlbumItem[]);
     const totalPages = computed(() => Math.ceil(totalItems.value / itemsPerPage.value));
     const firstIndex = computed(() => (pageNumber.value - 1) * itemsPerPage.value);
     const lastIndex = computed(() =>
@@ -78,7 +104,7 @@ export default defineComponent({
 
     watch(searchKey, () => {
       pageNumber.value = 1;
-      itemsPerPage.value = 10;
+      itemsPerPage.value = 20;
       getFilteredAlbumList();
     });
 
@@ -90,6 +116,8 @@ export default defineComponent({
       totalPages,
       itemsPerPage,
       chunkAlbumList,
+      albumListType,
+      setAlbumListType: (type: 'list' | 'square') => (albumListType.value = type),
     };
   },
 });
