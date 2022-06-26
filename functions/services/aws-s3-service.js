8c09c6498636cbbbcde30358b8fd6fef9ece2f0e
Laurence Ho <bluegray.he@aol.com>
2022-06-26 18:49:14 +1200

    Fix AWS cognito

--- functions/services/aws-s3-service.js
+++ functions/services/aws-s3-service.js
@@ -1,14 +1,21 @@
 const { S3Client, ListObjectsV2Command, UploadPartCommand } = require('@aws-sdk/client-s3');
 const { fromCognitoIdentityPool } = require('@aws-sdk/credential-provider-cognito-identity');
-const { CognitoIdentityClient } = require('@aws-sdk/client-cognito-identity');
+const {
+  CognitoIdentityClient,
+  GetIdCommand,
+  GetCredentialsForIdentityCommand,
+} = require('@aws-sdk/client-cognito-identity');
+
+const cognitoIdentityClient = new CognitoIdentityClient({ region: process.env.AWS_REGION });
 
 const fetchObjectFromS3 = async (folderName, maxKeys) => {
   const folderNameKey = encodeURIComponent(folderName) + '/';
+  // Use unauth identity
   const s3Client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: fromCognitoIdentityPool({
       client: new CognitoIdentityClient({ region: process.env.AWS_REGION }),
-      identityPoolId: process.env.AWS_PUBLIC_IDENTITY_POOL_ID,
+      identityPoolId: process.env.AWS_IDENTITY_POOL_ID,
     }),
   });
 
@@ -23,21 +30,41 @@ const fetchObjectFromS3 = async (folderName, maxKeys) => {
   return result?.Contents;
 };
 
-const uploadObject = (idToken, filePath, object) => {
+const _getIdFromCognito = async (idToken) => {
+  const command = new GetIdCommand({
+    IdentityPoolId: process.env.AWS_IDENTITY_POOL_ID,
+    Logins: {
+      'accounts.google.com': idToken,
+    },
+  });
+  const response = await cognitoIdentityClient.send(command);
+  return response.IdentityId;
+};
+
+const _getCredentials = async (identityId, idToken) => {
+  const command = new GetCredentialsForIdentityCommand({
+    IdentityId: identityId,
+    Logins: {
+      'accounts.google.com': idToken,
+    },
+  });
+  const response = await cognitoIdentityClient.send(command);
+  return response.Credentials;
+};
+
+const uploadObject = async (idToken, filePath, file) => {
+  console.log('##### file', file);
+  const identityId = await _getIdFromCognito(idToken);
+  const credentials = await _getCredentials(identityId, idToken);
+
   // FIXME
   const s3Client = new S3Client({
     region: process.env.AWS_REGION,
-    credentials: fromCognitoIdentityPool({
-      client: new CognitoIdentityClient({ region: process.env.AWS_REGION }),
-      identityPoolId: process.env.AWS_AUTH_IDENTITY_POOL_ID,
-      logins: {
-        'accounts.google.com': idToken,
-      },
-    }),
+    credentials,
   });
 
   const command = new UploadPartCommand({
-    Body: object,
+    Body: file,
     Bucket: process.env.AWS_S3_BUCKET_NAME,
     Key: filePath,
   });
