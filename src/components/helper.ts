7246a6eede3c3c91c6e1972675a2b37ee526b455
Laurence Ho <bluegray.he@aol.com>
2023-06-17 13:38:57 +1200

    Update CDN and npm dependencies

--- src/components/helper.ts
+++ src/components/helper.ts
@@ -1,4 +1,3 @@
 export const getS3Url = (photoKey: string): string => {
-  photoKey = photoKey.replace(' ', '+');
-  return `https://s3.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}/${photoKey}`;
+  return `${process.env.STATIC_FILES_URL}/${photoKey}`
 };
