501d959afa0cad4296a510a733ff85efb3aba2e3
Laurence Ho <bluegray.he@aol.com>
2023-12-28 08:32:18 +1300

    Update unit test

--- test/vitest/__tests__/mock-data.ts
+++ test/vitest/__tests__/mock-data.ts
@@ -1,4 +1,5 @@
 import { vi } from 'vitest';
+import { Album } from '../../../src/components/models';
 
 export const mockFetch = () => {
   const mockFetchPromise = Promise.resolve({
@@ -12,8 +13,15 @@ export const mockFetch = () => {
   globalRef.fetch = vi.fn().mockImplementation(() => mockFetchPromise);
 };
 
-export const mockAlbumList = [
-  { id: 'Sport', albumName: 'Sport', description: 'Sport', tags: ['sport'], isPrivate: false },
+export const mockAlbumTagList = [{ tag: 'sport' }, { tag: 'food' }, { tag: 'hiking' }];
+export const mockAlbumList: Album[] = [
+  {
+    id: 'Sport',
+    albumName: 'Sport',
+    description: 'Sport',
+    tags: ['sport'],
+    isPrivate: false,
+  },
   { id: 'Food', albumName: 'Food title', description: 'Food desc', tags: ['food', 'test'], isPrivate: false },
   { id: 'Hiking', albumName: 'Hiking', description: 'Hiking', tags: ['hiking'], isPrivate: false },
   { id: 'Shoes', albumName: 'Shoes', description: 'Shoes', tags: [], isPrivate: false },
@@ -23,6 +31,14 @@ export const mockAlbumList = [
     description: 'Do something secret',
     tags: ['secret'],
     isPrivate: true,
+    place: {
+      displayName: 'secret place',
+      formattedAddress: 'secret place',
+      location: {
+        latitude: 80,
+        longitude: 80,
+      },
+    },
   },
 ];
 
--- test/vitest/__tests__/stores/album-store.spec.ts
+++ test/vitest/__tests__/stores/album-store.spec.ts
@@ -1,7 +1,7 @@
 import { setActivePinia, createPinia } from 'pinia';
 import { describe, it, expect, beforeEach } from 'vitest';
 import { albumStore } from '../../../../src/stores/album-store';
-import { mockAlbumList } from '../mock-data';
+import { mockAlbumList, mockAlbumTagList } from '../mock-data';
 
 describe('Album Store', () => {
   beforeEach(() => {
@@ -18,16 +18,56 @@ describe('Album Store', () => {
   it('filteredAlbumList', () => {
     const store = albumStore();
     store.allAlbumList = mockAlbumList;
-    expect(store.filteredAlbumList('', [])).toEqual(mockAlbumList);
-    expect(store.filteredAlbumList('food', [])).toEqual([
+    expect(store.filteredAlbumList('', [], false)).toEqual(mockAlbumList);
+    expect(store.filteredAlbumList('food', [], false)).toEqual([
       { id: 'Food', albumName: 'Food title', description: 'Food desc', tags: ['food', 'test'], isPrivate: false },
     ]);
     expect(store.chunkAlbumList(0, 2)).toEqual([
       { id: 'Sport', albumName: 'Sport', description: 'Sport', tags: ['sport'], isPrivate: false },
       { id: 'Food', albumName: 'Food title', description: 'Food desc', tags: ['food', 'test'], isPrivate: false },
     ]);
+    // Filter private album
+    expect(store.filteredAlbumList('', [], true)).toEqual([
+      {
+        id: 'Do something secret',
+        albumName: 'Do something secret',
+        description: 'Do something secret',
+        tags: ['secret'],
+        isPrivate: true,
+        place: {
+          displayName: 'secret place',
+          formattedAddress: 'secret place',
+          location: {
+            latitude: 80,
+            longitude: 80,
+          },
+        },
+      },
+    ]);
     store.allAlbumList = [];
     expect(store.chunkAlbumList(0, 2)).toEqual([]);
+  });
+
+  it('albumsHaveLocation', () => {
+    const store = albumStore();
+    store.allAlbumList = mockAlbumList;
+    expect(store.albumsHaveLocation).toEqual([
+      {
+        id: 'Do something secret',
+        albumName: 'Do something secret',
+        description: 'Do something secret',
+        tags: ['secret'],
+        isPrivate: true,
+        place: {
+          displayName: 'secret place',
+          formattedAddress: 'secret place',
+          location: {
+            latitude: 80,
+            longitude: 80,
+          },
+        },
+      },
+    ]);
   });
 
   it('updateAlbumCover', () => {
@@ -41,5 +81,70 @@ describe('Album Store', () => {
       isPrivate: false,
     });
     expect(store.allAlbumList[0].albumName).toEqual('Sport Update');
+  });
+
+  it('updateAlbum', () => {
+    const store = albumStore();
+    store.allAlbumList = mockAlbumList;
+    // If album exists
+    store.updateAlbum(
+      {
+        id: 'Sport',
+        albumName: 'Sport Update',
+        description: 'Sport',
+        tags: ['sport'],
+        isPrivate: false,
+      },
+      false
+    );
+    expect(store.allAlbumList[0].albumName).toEqual('Sport Update');
+    expect(store.allAlbumList.length).toEqual(5);
+    // If album doesn't exist
+    store.updateAlbum(
+      {
+        id: 'Sport1',
+        albumName: 'Sport Update',
+        description: 'Sport',
+        tags: ['sport'],
+        isPrivate: false,
+      },
+      false
+    );
+    expect(store.allAlbumList.length).toEqual(6);
+    expect(store.allAlbumList.find((album) => album.id === 'Sport1')).not.toBeNull();
+    // Delete album
+    store.updateAlbum(
+      {
+        id: 'Sport1',
+        albumName: 'Sport Update',
+        description: 'Sport',
+        tags: ['sport'],
+        isPrivate: false,
+      },
+      true
+    );
+    expect(store.allAlbumList.length).toEqual(5);
+  });
+
+  it('updateAlbumTags', () => {
+    const store = albumStore();
+    store.albumTags = mockAlbumTagList;
+    expect(store.albumTags.length).toEqual(3);
+    // Add tag
+    store.updateAlbumTags(
+      {
+        tag: 'sport1',
+      },
+      false
+    );
+    expect(store.albumTags.length).toEqual(4);
+    // Delete tag
+    store.updateAlbumTags(
+      {
+        tag: 'sport1',
+      },
+      true
+    );
+    expect(store.albumTags.length).toEqual(3);
   });
 });
