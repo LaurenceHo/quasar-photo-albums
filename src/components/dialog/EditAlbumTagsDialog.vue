17d8df9657845a5b3981ad08b18f5c1c8bd83186
Laurence Ho <bluegray.he@aol.com>
2024-06-27 19:09:05 +1200

    Add test

--- src/components/dialog/EditAlbumTagsDialog.vue
+++ src/components/dialog/EditAlbumTagsDialog.vue
@@ -3,14 +3,28 @@
     <q-card :style="$q.screen.gt.xs ? 'min-width: 400px' : 'min-width: 360px'">
       <q-card-section class="flex justify-between">
         <div class="text-h6">Album tags</div>
-        <q-btn color="primary" icon="mdi-tag-plus" outline text-color="primary" @click="createTagDialog = true" />
+        <q-btn
+          color="primary"
+          icon="mdi-tag-plus"
+          outline
+          text-color="primary"
+          data-test-id="create-tag-button"
+          @click="createTagDialog = true"
+        />
       </q-card-section>
       <q-card-section class="scroll" style="max-height: 50vh">
         <q-list separator>
-          <q-item v-for="albumTag in albumTags" :key="albumTag.tag">
+          <q-item v-for="(albumTag, i) in albumTags" :key="albumTag.tag">
             <q-item-section>{{ albumTag.tag }}</q-item-section>
             <q-item-section side>
-              <q-btn color="primary" dense flat icon="mdi-delete" @click="openDeleteDialog(albumTag.tag)" />
+              <q-btn
+                :data-test-id="`delete-tag-button-${i}`"
+                color="primary"
+                dense
+                flat
+                icon="mdi-delete"
+                @click="openDeleteDialog(albumTag.tag)"
+              />
             </q-item-section>
           </q-item>
         </q-list>
@@ -18,13 +32,19 @@
     </q-card>
   </q-dialog>
 
-  <q-dialog v-model="createTagDialog" persistent transition-hide="scale" transition-show="scale">
+  <q-dialog
+    v-model="createTagDialog"
+    persistent
+    transition-hide="scale"
+    transition-show="scale"
+    data-test-id="create-tag-dialog"
+  >
     <q-card style="width: 300px">
       <q-card-section>
         <div class="text-h6">New tag</div>
       </q-card-section>
 
-      <q-form @submit.prevent.stop="confirmCreateTag" @reset.prevent.stop="onReset">
+      <q-form @submit.prevent="confirmCreateTag" @reset.prevent="onReset">
         <q-card-section class="q-pt-none scroll" style="max-height: 50vh">
           <q-input
             v-model="tagName"
@@ -34,6 +54,7 @@
             label="Tag"
             outlined
             stack-label
+            data-test-id="input-album-tag"
           />
         </q-card-section>
 
@@ -53,7 +74,7 @@
     </q-card>
   </q-dialog>
 
-  <q-dialog v-model="deleteTagDialog" persistent>
+  <q-dialog v-model="deleteTagDialog" persistent data-test-id="delete-tag-dialog">
     <q-card>
       <q-card-section class="row items-center">
         <q-icon color="primary" name="mdi-alert-circle" size="md" />
@@ -62,7 +83,15 @@
 
       <q-card-actions align="right">
         <q-btn v-close-popup :disable="isProcessing" color="primary" flat label="Cancel" no-caps />
-        <q-btn :loading="isProcessing" color="primary" label="Confirm" no-caps unelevated @click="confirmDeleteTag" />
+        <q-btn
+          data-test-id="confirm-delete-tag-button"
+          :loading="isProcessing"
+          color="primary"
+          label="Confirm"
+          no-caps
+          unelevated
+          @click="confirmDeleteTag"
+        />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -88,7 +117,6 @@ const tagName = ref('');
 
 const paramsYear = computed(() => route.params['year'] as string);
 const albumTags = computed(() => useAlbumStore.albumTags);
-
 const filteredAlbumsByYear: FilteredAlbumsByYear = JSON.parse(<string>LocalStorage.getItem(FILTERED_ALBUMS_BY_YEAR));
 
 const confirmCreateTag = async () => {
