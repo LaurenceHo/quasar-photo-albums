4ba60c5e0f1fee24bde16337775529abec0c9be4
Laurence Ho <bluegray.he@aol.com>
2025-12-03 10:13:44 +1300

    Tidy up lambda leftover

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import logger from 'pino';
-import { app } from './app.js';
-import { initialiseS3Bucket } from './services/initialise-s3-bucket.js';
-
-await initialiseS3Bucket();
-logger().info('Finish verifying S3 bucket.');
-
-await app.listen({ port: 3000 });
-logger().info('App is listening on port 3000.');
